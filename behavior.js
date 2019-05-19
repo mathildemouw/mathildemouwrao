37f66c5a0017871196607f1d2252d790a5f32880
Mathilde <nihaomouw@mac.com>
2019-05-19 16:00:04 -0700

    Add portfolio sections and initial JS

---
+++ behavior.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	listener = new PortfolioSectionListener();
+};
+
+function PortfolioSectionListener () {
+	console.log("HELOOOOO");
+	
+	console.log($(".portfolio-section"));
+}

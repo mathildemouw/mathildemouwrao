f436c49f70f50197cd13977f45486eb8741df8fb
Mathilde <nihaomouw@mac.com>
2019-05-21 21:55:29 -0700

    Fix some styling

--- behavior.js
+++ behavior.js
@@ -1,9 +1,31 @@
 window.onload = function () {
-	listener = new PortfolioSectionListener();
+	listener = new PortfolioSectionsListener();
 };
 
-function PortfolioSectionListener () {
-	console.log("HELOOOOO");
-	
-	console.log($(".portfolio-section"));
-}
+function PortfolioSectionsListener () {
+//  songwriting code opera moreart bio
+	$( "#songwriting-js" ).click(function() {
+		$( ".portfolio-section" ).hide();
+		$( ".songwriting" ).show();
+	})
+
+	$( "#code-js" ).click(function() {
+		$( ".portfolio-section" ).hide();
+		$( ".code" ).show();
+	})
+
+	$( "#opera-js" ).click(function() {
+		$( ".portfolio-section" ).hide();
+		$( ".opera" ).show();
+	})
+
+	$( "#moreart-js" ).click(function() {
+		$( ".portfolio-section" ).hide();
+		$( ".moreart" ).show();
+	})
+
+	$( "#bio-js" ).click(function() {
+		$( ".portfolio-section" ).hide();
+		$( ".bio" ).show();
+	})
+};
